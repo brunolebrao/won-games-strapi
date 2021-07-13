476022c246fffd5a5e495577236fa33e3884d8a6
Bruno Lebrao <bruno@DESKTOP-87RKSQI.localdomain>
2021-07-13 17:26:38 -0300

    initial projet strapi

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd445cb694df05a67933eefc237c05b5e'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a4fe63b4-d9b9-4567-9553-89dec508daf1'
+};
